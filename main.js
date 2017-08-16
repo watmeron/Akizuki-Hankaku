454edd065b85f7f8ef6b13e0c95f5c5e783e8c1e
watmeron <31073720+watmeron@users.noreply.github.com>
2017-08-17 01:12:49 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿
+function replaceZen2Han (n) {
+  if (n.nodeType === Node.TEXT_NODE) {
+    if (n.parentNode &&
+        n.parentNode.nodeName === 'TEXTAREA') {
+      return;
+    }
+
+    let content = n.textContent;
+
+		content = content.replace("　", " ");
+		content = content.replace(/[Ａ-Ｚ－／ａ-ｚ０-９]/g, function(s) {
+    	return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+		});
+
+    n.textContent = content;
+  }
+  else {
+    for (let i = 0; i < n.childNodes.length; i++) {
+      replaceZen2Han(n.childNodes[i]);
+    }    
+  }
+}
+
+replaceZen2Han(document.body);
